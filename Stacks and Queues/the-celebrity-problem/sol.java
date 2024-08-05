bbf46ee1d2d6fdc972194e9c0f09273986585ea9
Aratrika Mallick <aratrikamallick1603@gmail.com>
2024-08-05 21:16:26 +0530

    code commit

---
+++ Stacks and Queues/the-celebrity-problem/sol.java
@@ -0,0 +1,26 @@
+// import java.util.*;
+
+class Solution 
+{
+    public int celebrity(int mat[][]) 
+    {
+        // code here
+        int a = 0, b = mat.length - 1;
+        
+        while(a < b)
+        {
+            if(mat[a][b] == 1)
+                a++;
+            else
+                b--;
+        }
+        
+        for(int i = 0; i < mat.length; i++)
+        {
+            if((i != a) && (mat[a][i] == 1 || mat[i][a] == 0))
+                return -1;
+                
+        }
+        return a;
+    }
+}
